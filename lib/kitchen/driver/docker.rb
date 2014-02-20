31514b8f95a7e89176ddcc33ecf574558546d5cd
Eric Richardson <erichardson@emcien.com>
2014-02-20 15:46:51 -0500

    Look for ENV['DOCKER_HOST'] when setting default socket

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -26,7 +26,7 @@ module Kitchen
     # @author Sean Porter <portertech@gmail.com>
     class Docker < Kitchen::Driver::SSHBase
 
-      default_config :socket,        'unix:///var/run/docker.sock'
+      default_config :socket,        ENV['DOCKER_HOST'] || 'unix:///var/run/docker.sock'
       default_config :privileged,    false
       default_config :use_cache,     true
       default_config :remove_images, false
